c5f555d6e13bda40a0fb58402466362ac050a941
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-12 14:01:57 +0200

    add pipelines to run and a set of experiments
    - baseline verification pipeline to recreate
    data in the original mosso paper
    - tuning parameters for merging and divide
    strategies separately.

---
+++ pipeline/baseline_verification.sh
@@ -0,0 +1,3 @@
+./run.sh speed --runs 3 --group insertion dynamic --algorithm kdd20-mosso mags
+./run.sh cc --runs 3 --group insertion dynamic  --algorithm kdd20-mosso mags
+./run.sh scale --runs 3 --group insertion dynamic --algorithm kdd20-mosso mags
