ecd26cf86db3339af4ec4a6109057c92ff579057
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-14 18:30:17 +0200

    move mags and mags_dm to baseline pipeline and add parallel mags and mags_dm

--- pipeline/mags_vs_mags_dm.sh
+++
@@ -1,3 +0,0 @@
-./run.sh benchmark --runs 3 --group mags_small --algorithm mags mags_dm
-./run.sh benchmark --runs 3 --group mags_large --algorithm mags mags_dm
-./run.sh benchmark --runs 3 --group mags_large --algorithm para_mags para_mags_dm
