c4a92ac2cb3cd2daf5963ac40b0daddc8c8a9c85
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-15 14:35:16 +0200

    change threads to 30 to match cpu cores on remote server

--- pipeline/baseline_verification.sh
+++ pipeline/baseline_verification.sh
@@ -3,5 +3,5 @@
 ./run.sh scale --runs 3 --group insertion dynamic --algorithm kdd20-mosso mags mags_dm
 ./run.sh benchmark --runs 3 --group mags_small --algorithm mags mags_dm
 ./run.sh benchmark --runs 3 --group mags_large --algorithm mags mags_dm
-./run.sh benchmark --runs 3 --group mags_small --algorithm para_mags para_mags_dm --p 40
-./run.sh benchmark --runs 3 --group mags_large --algorithm para_mags para_mags_dm --p 40
+./run.sh benchmark --runs 3 --group mags_small --algorithm para_mags para_mags_dm --p 30
+./run.sh benchmark --runs 3 --group mags_large --algorithm para_mags para_mags_dm --p 30
