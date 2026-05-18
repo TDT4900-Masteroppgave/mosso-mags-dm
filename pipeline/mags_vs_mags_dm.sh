b2947c21bcd0ad2abb23cc93b70e994cd3c4cfa8
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-18 15:38:26 +0200

    fix parallelisation issues in pipeline scripts

--- pipeline/mags_vs_mags_dm.sh
+++ pipeline/mags_vs_mags_dm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./run.sh benchmark --runs 3 --group mags_small mags_larg --algorithm mags mags_dm para_mags para_mags_dm --p 30
+./run.sh benchmark --runs 3 --group mags_small mags_large --algorithm mags mags_dm para_mags para_mags_dm --p 30
