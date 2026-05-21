9a2603268e16d4eecb99402f114a28990df9dbfb
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-21 21:01:13 +0200

    add mags to full_solution_verification.sh

--- pipeline/full_solution_verification.sh
+++ pipeline/full_solution_verification.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./run.sh speed --runs 3 --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso proposed
-./run.sh cc --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso proposed
-./run.sh scale --dataset SK EU --algorithm kdd20-mosso proposed
+./run.sh speed --runs 3 --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso proposed kags
+./run.sh cc --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso proposed mags
+./run.sh scale --dataset SK EU --algorithm kdd20-mosso proposed mags
 ./run.sh benchmark --runs 3 --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso proposed
