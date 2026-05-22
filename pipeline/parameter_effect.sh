2b3f2ffe2701519fe286c8f3878a2ec7bbb41a31
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-22 15:45:27 +0200

    update parameter to include T at 1 and cap at 0

--- pipeline/parameter_effect.sh
+++ pipeline/parameter_effect.sh
@@ -12,16 +12,18 @@ for c_val in "${SAMPLE_SIZES[@]}"; do
     ./run.sh sweep --algorithm sm --dataset PR EN FB DB YT --dynamic DB YT --param h --range 1 1 1 --c "$c_val" &
     sleep 2
 
-    ./run.sh sweep --algorithm sm_thr --dataset PR EN FB DB YT --dynamic DB YT --param thr --range 0.1 1.1 0.1 --c "$c_val" &
+    ./run.sh sweep --algorithm sm_thr --dataset PR EN FB DB YT --dynamic DB YT --param thr --range 0.0 1.1 0.1 --c "$c_val" &
     sleep 2
 
     ./run.sh sweep --algorithm top-b --dataset PR EN FB DB YT --dynamic DB YT --param b --range 1 10 1 --c "$c_val" &
     sleep 2
 
-    ./run.sh sweep --algorithm cap --dataset PR EN FB DB YT --dynamic DB YT --param cap --range 10 120 10 --c "$c_val" &
+    ./run.sh sweep --algorithm cap --dataset PR EN FB DB YT --dynamic DB YT --param cap --range 0 120 10 --c "$c_val" &
     sleep 2
 
-    ./run.sh sweep --algorithm ds_thr --dataset PR EN FB DB YT --dynamic DB YT --param T --range 1 100 10 --c "$c_val" &
+    ./run.sh sweep --algorithm ds_thr --dataset PR EN FB DB YT --dynamic DB YT --param T --range 10 100 10 --c "$c_val" &
+    sleep 2
+    ./run.sh sweep --algorithm ds_thr --dataset PR EN FB DB YT --dynamic DB YT --param T --range 1 1 1 --c "$c_val" &
 
     # Wait for all background sweeps of the current sample size to finish before moving to the next
     wait
