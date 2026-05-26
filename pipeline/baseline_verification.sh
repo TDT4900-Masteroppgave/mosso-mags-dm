d4fe5e6f1d782bcc29e85f749b04bacc6c7fc771
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-26 08:53:57 +0200

    remove baseline verifcation and use full_solution verification instead

--- pipeline/baseline_verification.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./run.sh speed --runs 3 --group insertion dynamic --algorithm kdd20-mosso mags
-./run.sh cc --group insertion dynamic  --algorithm kdd20-mosso mags
-./run.sh scale --dataset SK EU --algorithm kdd20-mosso mags
--- pipeline/full_solution_verification.sh
+++ pipeline/full_solution_verification.sh
@@ -2,4 +2,4 @@
 ./run.sh speed --runs 3 --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso full_solution mags
 ./run.sh cc --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso full_solution mags
 ./run.sh scale --dataset SK EU --algorithm kdd20-mosso full_solution mags
-./run.sh benchmark --runs 3 --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso full_solution
+./run.sh benchmark --runs 3 --group insertion dynamic --dynamic DB YT SK LJ --algorithm kdd20-mosso full_solution mags
