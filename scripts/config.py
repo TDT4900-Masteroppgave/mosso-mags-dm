26b0735940054b4159d43a9a98201ab3a577920b
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-05-26 09:33:24 +0200

    update config with new parameter bounds

--- scripts/config.py
+++ scripts/config.py
@@ -8,18 +8,15 @@ ALGORITHMS_DIR = Path("algorithms")
 BASE_REPO_URL = "https://github.com/TDT4900-Masteroppgave/mosso-mags-dm.git"
 
 PARAM_CONFIG = {
-    "c": {"description": "sample number", "default": 120, "bounds": (10, 120), "step": 10, "type": int},
+    "c": {"description": "sample number", "default": 120, "bounds": (30, 240), "step": 10, "type": int},
     "e": {"description": "escape", "default": 3, "bounds": (1, 5), "step": 1, "type": int},
     "interval": {"description": "interval", "default": 1000, "type": int},
-    "b": {"description": "top candidates", "default": 1, "bounds": (1, 3), "step": 1, "type": int},
-    #"b": {"description": "top candidates", "default": 1, "bounds": (1, 5), "step": 1, "type": int},
+    "b": {"description": "top candidates", "default": 1, "bounds": (1, 5), "step": 1, "type": int},
     "h": {"description": "hashes", "default": 4, "bounds": (1, 5), "step": 1, "type": int},
     #"h": {"description": "hashes", "default": 4, "bounds": (1, 50), "step": 10, "type": int},
     "thr_start": {"description": "start threshold", "default": 1.0, "type": float},
-    "thr": {"description": "end threshold", "default": 0.0, "bounds": (0.0, 0.5), "step": 0.10, "type": float},
-    #"thr": {"description": "end threshold", "default": 0.0, "bounds": (0.0, 0.5), "step": 0.05, "type": float},
-    "cap": {"description": "cap of coarse cluster buckets", "default": 60, "bounds": (0, 120), "step": 20,
-    #"cap": {"description": "cap of coarse cluster buckets", "default": 60, "bounds": (10, 120), "step": 10,
+    "thr": {"description": "end threshold", "default": 0.0, "bounds": (0.0, 0.5), "step": 0.05, "type": float},
+    "cap": {"description": "cap of coarse cluster buckets", "default": 60, "bounds": (10, 120), "step": 10,
             "type": int},
     "T": {"description": "iterations over partitions", "default": 1, "bounds": (1, 10), "step": 10, "type": int},
     "p": {"description": "number of threads", "default": 30, "type": int},
